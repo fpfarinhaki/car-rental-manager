b79cdf122dc72efeb1016a083bbd0d11118ebe5d
Felipe P. Farinhaki <ffarinhaki@gmail.com>
2019-06-27 23:48:21 +0200

    Changes to client registration form

--- src/app/modal/add-client-modal/add-client-modal.page.ts
+++ src/app/modal/add-client-modal/add-client-modal.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControlOptions, FormBuilder } from '@angular/forms';
 import { Client, ClientService } from 'src/app/services/client.service';
 
 @Component({
@@ -11,24 +11,35 @@ import { Client, ClientService } from 'src/app/services/client.service';
 export class AddClientModalPage implements OnInit {
 
   clientForm: FormGroup;
+  valid: boolean;
 
-  constructor(private modalController: ModalController, 
-    private clientService: ClientService) { 
-    this.clientForm = new FormGroup({
-      name : new FormControl('', Validators.required),
+  constructor(private modalController: ModalController,
+              private clientService: ClientService,
+              private formBuilder: FormBuilder) {
+  }
+
+  onFormStatusChange() {
+    this.clientForm.statusChanges
+      .subscribe((event) => {
+        console.log(event);
+      });
+  }
+
+  ngOnInit() {
+    this.clientForm = this.formBuilder.group({
+      name : new FormControl('', {updateOn: 'change', validators: Validators.required}),
       surname_1: new FormControl('', Validators.required),
       surname_2: new FormControl(''),
       payment: new FormControl('Seleciona el pagamento', Validators.required)
     });
-  }
 
-  ngOnInit() {
+    this.onFormStatusChange();
   }
 
   onSubmit() {
     console.log(this.clientForm.value);
     const value = this.clientForm.value;
-    const client = new Client(value['name'], value['payment']);
+    const client = new Client(value.name, value.payment);
     console.log(client);
     this.clientService.addClient(client);
     this.modalController.dismiss();
