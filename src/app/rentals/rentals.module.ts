bd66875756a292a3dd421c2fddef180948e243fc
felipe <fpfarinhaki.atc@gmail.com>
2019-06-27 19:45:06 +0200

    Rename tab1 to rentals

--- src/app/rentals/rentals.module.ts
+++ src/app/rentals/rentals.module.ts
@@ -3,15 +3,15 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab1Page } from './tab1.page';
+import { RentalsPage } from './rentals.page';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{ path: '', component: RentalsPage }])
   ],
-  declarations: [Tab1Page]
+  declarations: [RentalsPage]
 })
-export class Tab1PageModule {}
+export class RentalsPageModule {}
---
+++ src/app/rentals/rentals.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rentals',
+  templateUrl: 'rentals.page.html',
+  styleUrls: ['rentals.page.scss']
+})
+export class RentalsPage {
+
+  constructor() {}
+
+}
